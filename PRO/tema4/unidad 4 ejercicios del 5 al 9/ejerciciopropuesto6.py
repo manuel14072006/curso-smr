065ee6e6f08a690da97340730c177d27be1f5d59
manuel20061407 <manand2@alu.edu.gva.es>
2025-12-12 14:50:45 +0100

    ejecicio terminado

---
+++ PRO/tema4/unidad 4 ejercicios del 5 al 9/ejerciciopropuesto6.py
@@ -0,0 +1,11 @@
+
+meses= ["enero", "febrero", "marzo", "abril", "mayo", "junio", "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre" ]
+
+dias=[31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+numero=int(input("introduce un numero de mes: "))
+
+if 1 <= numero <=12:
+    print(f"el mes {meses[numero - 1]} tiene {dias[numero - 1]} dias.")
+else:
+    print("unumero de mes invalido.")
