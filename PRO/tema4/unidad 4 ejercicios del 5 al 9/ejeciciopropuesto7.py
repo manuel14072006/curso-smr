83ed9f619371d255a86ba014efe16a4b3774e278
manuel20061407 <manand2@alu.edu.gva.es>
2025-12-12 16:16:25 +0100

    ejecicio terminado

---
+++ PRO/tema4/unidad 4 ejercicios del 5 al 9/ejeciciopropuesto7.py
@@ -0,0 +1,24 @@
+
+alumnos = []
+
+while True:
+
+    nombre = (input("introduce tu nombre: "))
+    if nombre == "-":
+        break
+    edad = int(input("introduce tu edad"))
+
+    alumnos.append((nombre, edad))
+
+print("resultado")
+
+print("mayores de edad:")
+for nombre, edad in alumnos:
+  if edad >= 18:
+    print(f"{nombre} ({edad}) años")
+
+alumnos_ordenados = sorted(alumnos, key=lambda x: x[1], reverse=True)
+
+print ("los dos alumnos mas mayores: ")
+for nombre, edad in alumnos_ordenados[:2]:
+  print(f"{nombre} ({edad} años)")
