e5264036f02926d56bb86305651ec0f2711653f9
manuel20061407 <manand2@alu.edu.gva.es>
2026-01-09 16:13:19 +0100

    ejercicio propuesto 10

---
+++ PRO/tema4/unidad 4 ejercicios del 10 al 12/ejerciciopropuesto10.py
@@ -0,0 +1,17 @@
+x = ('C', 'h', 'R', 'm', 'A', 's', 'M', 'o', 'T', 'y', 'c')
+
+resultado = x [:-1] + ('d','e')
+
+mayus = []
+minus = []
+
+for letra in x:
+    if letra.isupper():
+        mayus.append()
+    else:
+        minus.append()
+
+
+print("Tupla original:", x)
+
+print("Tupla resultado:", resultado)
